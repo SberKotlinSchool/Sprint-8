3c6ac29b4b9c60e88b813b8712c43fcd5c2d1fe4
Michael <michaelmv@mail.ru>
2022-12-13 21:00:07 +0300

    Michael - решение задачи по теме Hystrix

---
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/dto/Pokemon.kt
@@ -0,0 +1,26 @@
+package ru.sberschool.hystrix.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Pokemon(
+    @JsonProperty("id")
+    val id: Int?,
+
+    @JsonProperty("name")
+    val name: String?,
+
+    @JsonProperty("base_experience")
+    val baseExperience: Int? = null,
+
+    @JsonProperty("height")
+    val height: Int? = null,
+
+    @JsonProperty("is_default")
+    val isDefault: Boolean? = null,
+
+    @JsonProperty("order")
+    val order: Int? = null,
+
+    @JsonProperty("weight")
+    val weight: Int? = null
+)
--- hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
+++ hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
@@ -7,25 +7,28 @@ import feign.jackson.JacksonDecoder
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockserver.client.server.MockServerClient
 import org.mockserver.integration.ClientAndServer
-import org.mockserver.model.HttpRequest
-import org.mockserver.model.HttpResponse
+import ru.sberschool.hystrix.dto.Pokemon
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 
 class SlowlyApiTest {
-    val client = HystrixFeign.builder()
+    private val fallbackClient = HystrixFeign.builder()
         .client(ApacheHttpClient())
         .decoder(JacksonDecoder())
-        // для удобства тестирования задаем таймауты на 1 секунду
         .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
         .target(SlowlyApi::class.java, "http://127.0.0.1:18080", FallbackSlowlyApi())
-    lateinit var mockServer: ClientAndServer
+
+    private val realClient = HystrixFeign.builder()
+        .client(ApacheHttpClient())
+        .decoder(JacksonDecoder())
+        .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
+        .target(SlowlyApi::class.java, "https://pokeapi.co/api/v2", FallbackSlowlyApi())
+
+    private lateinit var mockServer: ClientAndServer
 
     @BeforeEach
     fun setup() {
-        // запускаем мок сервер для тестирования клиента
         mockServer = ClientAndServer.startClientAndServer(18080)
     }
 
@@ -35,22 +38,19 @@ class SlowlyApiTest {
     }
 
     @Test
-    fun `getSomething() should return predefined data`() {
-        // given
-        MockServerClient("127.0.0.1", 18080)
-            .`when`(
-                // задаем матчер для нашего запроса
-                HttpRequest.request()
-                    .withMethod("GET")
-                    .withPath("/")
-            )
-            .respond(
-                // наш запрос попадает на таймаут
-                HttpResponse.response()
-                    .withStatusCode(400)
-                    .withDelay(TimeUnit.SECONDS, 30) //
-            )
-        // expect
-        assertEquals("predefined data", client.getSomething().data)
+    fun `getPokemon() returns real data`() {
+        val result = realClient.getPokemon()
+
+        assertEquals(35, result.id)
+        assertEquals("clefairy", result.name)
+        assertEquals(6, result.height)
+        assertEquals(75, result.weight)
+    }
+
+    @Test
+    fun `getPokemon() returns predefined data`() {
+        val result = fallbackClient.getPokemon()
+
+        assertEquals(Pokemon(-1, "Too long to wait"), result)
     }
 }
