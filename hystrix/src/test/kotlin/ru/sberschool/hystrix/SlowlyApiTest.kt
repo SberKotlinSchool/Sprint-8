d1b25cc08a7fc66d5bb54e656ec3e24682a52a75
Michael <michaelmv@mail.ru>
2022-12-14 20:48:11 +0300

    Michael - доработал тест на работоспособность клиента

--- hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
+++ hystrix/src/test/kotlin/ru/sberschool/hystrix/SlowlyApiTest.kt
@@ -16,17 +16,11 @@ import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
 
 class SlowlyApiTest {
-    private val fallbackClient = HystrixFeign.builder()
+    private val client = HystrixFeign.builder()
         .client(ApacheHttpClient())
         .decoder(JacksonDecoder())
         .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
         .target(SlowlyApi::class.java, "http://127.0.0.1:18080", FallbackSlowlyApi())
-
-    private val realClient = HystrixFeign.builder()
-        .client(ApacheHttpClient())
-        .decoder(JacksonDecoder())
-        .options(Request.Options(1, TimeUnit.SECONDS, 1, TimeUnit.SECONDS, true))
-        .target(SlowlyApi::class.java, "https://pokeapi.co/api/v2", FallbackSlowlyApi())
 
     private lateinit var mockServer: ClientAndServer
 
@@ -42,32 +36,36 @@ class SlowlyApiTest {
 
     @Test
     fun `getPokemon() returns real data`() {
-        val result = realClient.getPokemon()
-
-        assertEquals(35, result.id)
-        assertEquals("clefairy", result.name)
-        assertEquals(6, result.height)
-        assertEquals(75, result.weight)
-    }
-
-    @Test
-    fun `getPokemon() returns predefined data`() {
-        // given
         MockServerClient("127.0.0.1", 18080)
             .`when`(
-                // задаем матчер для нашего запроса
                 HttpRequest.request()
                     .withMethod("GET")
                     .withPath("/pokemon/35")
             )
             .respond(
-                // наш запрос попадает на таймаут
+                HttpResponse.response()
+                    .withStatusCode(200)
+                    .withDelay(TimeUnit.SECONDS, 0)
+                    .withBody("{\"id\": 35, \"name\": \"Pokemon\'s Name\", \"height\": 6, \"weight\": 75}")
+            )
+
+        assertEquals(Pokemon(35, "Pokemon's Name", height = 6, weight = 75), client.getPokemon())
+    }
+
+    @Test
+    fun `getPokemon() returns predefined data`() {
+        MockServerClient("127.0.0.1", 18080)
+            .`when`(
+                HttpRequest.request()
+                    .withMethod("GET")
+                    .withPath("/pokemon/35")
+            )
+            .respond(
                 HttpResponse.response()
                     .withStatusCode(400)
-                    .withDelay(TimeUnit.SECONDS, 30) //
+                    .withDelay(TimeUnit.SECONDS, 5) //
             )
-        // expect
 
-        assertEquals(Pokemon(-1, "Too long to wait"), fallbackClient.getPokemon())
+        assertEquals(Pokemon(-1, "Too long to wait"), client.getPokemon())
     }
 }
