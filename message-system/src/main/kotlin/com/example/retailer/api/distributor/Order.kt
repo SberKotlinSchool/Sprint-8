553d77eac60ff6dade117a9234292dc34bfc8bc3
PavelPopov4Kotlin <palepopov@sberbank.ru>
2022-12-18 23:43:04 +0600

    message-system resolved

--- message-system/src/main/kotlin/com/example/retailer/api/distributor/Order.kt
+++ message-system/src/main/kotlin/com/example/retailer/api/distributor/Order.kt
@@ -1,12 +1,26 @@
 package com.example.retailer.api.distributor
 
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
 /**
  * Описание заказа
  */
+@Entity
+@Table(name="order_")
 data class Order(
     /**
      * Уникальный идентификатор заказа на стороне ретейлера
      */
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     val id: String?,
 
     /**
@@ -22,5 +36,6 @@ data class Order(
     /**
      * Список заказанных товаров
      */
+    @OneToMany(cascade = [CascadeType.ALL])
     val items: List<Item>
 )
--- message-system/src/main/kotlin/com/example/retailer/api/distributor/OrderInfo.kt
+++ message-system/src/main/kotlin/com/example/retailer/api/distributor/OrderInfo.kt
@@ -1,8 +1,15 @@
 package com.example.retailer.api.distributor
 
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Id
+
 /**
  * Уведомление об изменении заказа
  */
+@Entity
 data class OrderInfo(
 
     /**
@@ -10,6 +17,7 @@ data class OrderInfo(
      *
      * @see com.example.retailer.api.distributor.Item#id
      */
+    @Id
     val orderId: String,
 
     /**
@@ -17,11 +25,14 @@ data class OrderInfo(
      *  Created
      *
      */
+    @Column
+    @Enumerated(value = EnumType.STRING)
     var status: OrderStatus,
 
     /**
      * Контрольная сумма
      */
+    @Column
     val signature: String,
 
 )
