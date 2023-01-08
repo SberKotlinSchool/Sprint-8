5d4ba7b006f55b8e5d58cba20e531e4ce5e15f36
Maria Starikova <starikova.mariya.v@gmail.com>
2023-01-08 22:47:29 +0300

    sprint 7

--- message-system/src/main/kotlin/com/example/retailer/api/distributor/Item.kt
+++ message-system/src/main/kotlin/com/example/retailer/api/distributor/Item.kt
@@ -1,16 +1,25 @@
 package com.example.retailer.api.distributor
 
+import java.io.Serializable
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
 /**
  * Описание товара
  */
+@Entity
 data class Item(
     /**
      * Произвольный идентификатор
      */
+    @Id
     val id: Long,
 
     /**
      * Произвольное название
      */
+    @Column(nullable = false)
     val name: String
-)
+): Serializable
