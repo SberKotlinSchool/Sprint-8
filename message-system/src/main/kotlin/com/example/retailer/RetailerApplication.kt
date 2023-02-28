1077bcda7b70a284fa1e95a4247a1d9bd0883021
Artem Astafev <astafev.ph@gmail.com>
2023-03-01 01:23:21 +0300

    Message-system - ok

--- message-system/src/main/kotlin/com/example/retailer/RetailerApplication.kt
+++ message-system/src/main/kotlin/com/example/retailer/RetailerApplication.kt
@@ -2,8 +2,10 @@ package com.example.retailer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class RetailerApplication
 
 fun main(args: Array<String>) {
