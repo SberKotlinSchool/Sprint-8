39c46851a158d302fe2ff65f2e86007cd4ef8222
Artem Astafev <astafev.ph@gmail.com>
2023-03-01 01:00:37 +0300

    правка конфигурации для RabbitMq

--- message-system/src/main/kotlin/com/example/retailer/config/RabbitMqConfig.kt
+++ message-system/src/main/kotlin/com/example/retailer/config/RabbitMqConfig.kt
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 @EnableRabbit
 class RabbitMqConfig(
-    @Value("\${rabbit.exchange.name}")
+    @Value("\${rabbitmq.exchange.name}")
     private val rabbitExchangeName: String,
 
     @Value("\${consumer.routing.prefix}")
