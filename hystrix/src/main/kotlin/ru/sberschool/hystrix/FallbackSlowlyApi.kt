55cd658274aefb657dab0a6550c67b822e067722
Slavan99 <99ken@mail.ru>
2023-12-19 22:33:38 +0300

    Sprint-7 hystrix

--- hystrix/src/main/kotlin/ru/sberschool/hystrix/FallbackSlowlyApi.kt
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/FallbackSlowlyApi.kt
@@ -1,7 +1,7 @@
 package ru.sberschool.hystrix
 
 class FallbackSlowlyApi : SlowlyApi {
-    override fun getSomething() = SimpleResponse("predefined data")
+    override fun getBerry(id: Int) = Berry(-1, "fallback berry")
 }
 
 
