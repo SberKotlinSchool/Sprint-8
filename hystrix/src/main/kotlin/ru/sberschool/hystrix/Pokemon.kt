274a383c5a08cd035cdbd3229ab21944309a5636
Красненков Роман Сергеевич <RSKrasnenkov@sberbank.ru>
2023-12-29 19:56:06 +0300

    Задание по теме "Паттерны микросервисной архитектуры"

---
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/Pokemon.kt
@@ -0,0 +1,6 @@
+package ru.sberschool.hystrix
+
+data class Pokemon(
+        val name: String?,
+        val url: String?
+)
---
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/PokemonPage.kt
@@ -0,0 +1,8 @@
+package ru.sberschool.hystrix
+
+data class PokemonPage(
+        val count: Int,
+        val next: String?,
+        val previous: String?,
+        val results: List<Pokemon> = emptyList()
+)
