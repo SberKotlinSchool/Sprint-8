78d4a27a8746deda572bb0b215d722529134cf33
Alexander Gagarin <sacred@xaker.ru>
2024-01-14 23:22:08 +0300

    Gagarin Homework #7

---
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/FallbackPokemonApi.kt
@@ -0,0 +1,10 @@
+package ru.sberschool.hystrix
+
+import ru.sberschool.hystrix.dto.Pokemon
+
+class FallbackPokemonApi : PokemonApi {
+
+    override fun getPokemon(id: Int): Pokemon = Pokemon()
+
+    override fun getPokemon(name: String): Pokemon = Pokemon()
+}
--- hystrix/src/main/kotlin/ru/sberschool/hystrix/FallbackSlowlyApi.kt
+++
@@ -1,7 +0,0 @@
-package ru.sberschool.hystrix
-
-class FallbackSlowlyApi : SlowlyApi {
-    override fun getSomething() = SimpleResponse("predefined data")
-}
-
-
