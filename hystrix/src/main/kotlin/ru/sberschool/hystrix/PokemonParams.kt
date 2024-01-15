9548209d503148a0579f95b77640dfc1b082eebd
ivan <strateg8888@gmail.com>
2024-01-16 00:09:40 +0400

    hystrix hw

---
+++ hystrix/src/main/kotlin/ru/sberschool/hystrix/PokemonParams.kt
@@ -0,0 +1,23 @@
+package ru.sberschool.entity
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class PokemonParams (
+    @JsonProperty("id")
+    val id: Long?,
+
+    @JsonProperty("name")
+    val name: String?,
+
+    @JsonProperty("order")
+    val order: Int? = null,
+
+    @JsonProperty("base_experience")
+    val baseExperience: Int? = null,
+
+    @JsonProperty("height")
+    val height: Int? = null,
+
+    @JsonProperty("weight")
+    val weight: Int? = null
+)
